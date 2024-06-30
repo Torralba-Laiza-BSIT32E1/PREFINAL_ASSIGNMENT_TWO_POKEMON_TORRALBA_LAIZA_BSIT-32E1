fa986160f9fbfcb5c5be538619783a5be6e3904d
Torralba_Laiza_BSIT32E1 <160841939+Torralba-Laiza-BSIT32E1@users.noreply.github.com>
2024-06-30 19:14:52 +0800

    Pokemon and index.cshtml

---
+++ TWO_POKEMON_TORRALBA_LAIZA_BSIT_32E1/Models/Pokemon.cs
@@ -0,0 +1,9 @@
+﻿namespace TWO_POKEMON_TORRALBA_LAIZA_BSIT_32E1.Models
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public List<string> Abilities { get; set; }
+        public List<string> Moves { get; set; }
+    }
+}
--- TWO_POKEMON_TORRALBA_LAIZA_BSIT_32E1/obj/Debug/net8.0/TWO_POKEMON_TORRALBA_LAIZA_BSIT_32E1.AssemblyInfo.cs
+++ TWO_POKEMON_TORRALBA_LAIZA_BSIT_32E1/obj/Debug/net8.0/TWO_POKEMON_TORRALBA_LAIZA_BSIT_32E1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TWO_POKEMON_TORRALBA_LAIZA_BSIT_32E1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04ffe216a794ed434299d28a25e374fee05ba287")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46957e0203549cc8f267d76ece8b3f17da409156")]
 [assembly: System.Reflection.AssemblyProductAttribute("TWO_POKEMON_TORRALBA_LAIZA_BSIT_32E1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TWO_POKEMON_TORRALBA_LAIZA_BSIT_32E1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
